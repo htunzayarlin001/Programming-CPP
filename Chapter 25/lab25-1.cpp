b74459304211e6ce674611c049289933c96d4e44
htunzayarlin001 <684244111@mail.pbru.ac.th>
2025-08-05 11:11:40 +0700

    Update lab25-1.cpp

--- Chapter 25/lab25-1.cpp
+++ Chapter 25/lab25-1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cmath>
 
+class Circle {
+public:
+    double radius;
+
+    Circle(double rr) {
+        radius = rr;
+    }
+
+    void findArea() {
+        std::cout << "Area of circle is " << (M_PI * radius * radius) << std::endl;
+    }
+
+    void perimeter() {
+        std::cout << "Perimeter of circle is " << (2.0 * M_PI * radius) << std::endl;
+    }
+};
+
+int main() {
+    Circle c(20);
+
+    c.findArea();
+    c.perimeter();
+
+    return 0;
+}
